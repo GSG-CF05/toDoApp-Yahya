5c935f737802d20d52fe2bea093c5e48f7a41715
yahya-1 <y.a.m.k123456@hotmail.com>
2022-03-05 18:04:27 +0300

    After modifications to the script file in HTML file relates #11

--- script.js
+++ script.js
@@ -31,12 +31,12 @@ function showTask() {
   let styleList = "";
   arr.forEach((element, index) => {
     styleList += `<li>
-                  <p>${element}</p>
-                  <span>
-                    <button class="button-edit" onclick="editItem(${index})"><img src="./icon/edit-svgrepo-com.svg"></img></button>
-                    <button class="button-delete" onclick="deleteItem(${index})"><img src="./icon/delete.svg"></img></button>
-                  </span>
-                </li>`;
+                <p>${element}</p>
+                <span>
+                  <button class="button-edit" onclick="editItem(${index})"><img src="./icon/edit-svgrepo-com.svg"></img></button>
+                  <button class="button-delete" onclick="deleteItem(${index})"><img src="./icon/delete.svg"></img></button>
+                </span>
+              </li>`;
   });
 
   listToDo.innerHTML = styleList;
@@ -58,3 +58,30 @@ function deleteAllItem() {
   localStorage.setItem("to do", JSON.stringify(arr));
   showTask();
 }
+
+function editItem(index) {
+  let saveItem = document.querySelector(".saveItem");
+  let addBton = document.querySelector(".add-icon");
+  let saveBtn = document.querySelector(".check-input");
+  let getLocalStorge = localStorage.getItem("to do");
+  arr = JSON.parse(getLocalStorge);
+  inputValue.value = arr[index];
+  saveItem.value = index;
+  addBton.style.display = "none";
+  saveBtn.style.display = "block";
+}
+
+let saveBtn = document.querySelector(".check-input");
+
+saveBtn.addEventListener("click", function () {
+  let addBton = document.querySelector(".add-icon");
+  let saveBtn = document.querySelector(".check-input");
+  let getLocalStorge = localStorage.getItem("to do");
+  arr = JSON.parse(getLocalStorge);
+  let saveItem = document.querySelector(".saveItem").value;
+  arr[saveItem] = inputValue.value;
+  saveBtn.style.display = "none";
+  addBton.style.display = "block";
+  localStorage.setItem("to do", JSON.stringify(arr));
+  showTask();
+});
